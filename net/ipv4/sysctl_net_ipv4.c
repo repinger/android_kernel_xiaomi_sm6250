ee6e03967a26f84c2f10c79fe4aafaaaebc31193
madaidan <50278627+madaidan@users.noreply.github.com>
2020-05-08 00:52:45 +0200

    net: tcp: add option to disable TCP simultaneous connect
    This is modified from Brad Spengler/PaX Team's code in the last public
    patch of grsecurity/PaX based on my understanding of the code. Changes
    or omissions from the original code are mine and don't reflect the
    original grsecurity/PaX code.
    
    TCP simultaneous connect adds a weakness in Linux's implementation of
    TCP that allows two clients to connect to each other without either
    entering a listening state. The weakness allows an attacker to easily
    prevent a client from connecting to a known server provided the source
    port for the connection is guessed correctly.
    
    As the weakness could be used to prevent an antivirus or IPS from
    fetching updates, or prevent an SSL gateway from fetching a CRL, it
    should be eliminated.
    
    This creates a net.ipv4.tcp_simult_connect sysctl that when disabled,
    disables TCP simultaneous connect.
    
    Reviewed-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    Reviewed-by: Levente Polyak <levente@leventepolyak.net>
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>
    Signed-off-by: repinger <devel@repinger.my.id>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -736,7 +736,15 @@ static struct ctl_table ipv4_table[] = {
 		.extra1		= &tcp_use_userconfig_min,
 		.extra2		= &tcp_use_userconfig_max,
 	},
-
+	{
+		.procname	= "tcp_simult_connect",
+		.data		= &sysctl_tcp_simult_connect,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
 	{ }
 };
 
